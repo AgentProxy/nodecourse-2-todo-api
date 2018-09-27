b1d8ad8354f52e3475877e333f4da4e4bc57472f
Eric Flores <ericjoseph.flores1@gmail.com>
2018-09-27 16:31:19 +0800

    palying with ports

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://EJFlow32:flores888777666555@ds115193.mlab.com:15193/testdatabase' || 'mongodb://localhost:27017/TodoApp', {
+mongoose.connect('mongodb://EJFlow32:flores888777666555@ds115193.mlab.com:15193/testdatabase', {
     useNewUrlParser: true
 });
 module.exports = {mongoose}
