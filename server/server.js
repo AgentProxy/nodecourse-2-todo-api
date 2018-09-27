eed94afcbcadeb9f8bd4682e30968e3d70b45d3c
Eric Flores <ericjoseph.flores1@gmail.com>
2018-09-27 15:16:55 +0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 const {mongoose} = require('../db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
@@ -26,6 +26,32 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     })
+});
+
+var notFound = (res) => {
+    res.status(400).send();
+}
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        console.log("Id is not valid");
+        return notFound(res);
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            console.log("No todo is found");
+            return notFound(res);
+        }
+        res.send({todo});
+    }).catch((e) => {
+        console.log("Error in finding ID");
+        return notFound(res);
+    });
+    //respond with 404 if invalid, send back empty body
+    //findbyid: success(send it back, if no (send back 404 with empty body)) and error case(400 and send empty body)
+    // res.send(req.params);
 });
 
 app.listen(4000, ()=>{
