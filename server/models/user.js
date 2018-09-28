8eaec9f16a49ea78814e74eb2831eeabfc915dc4
Eric Flores <ericjoseph.flores1@gmail.com>
2018-09-28 17:33:37 +0800

    Add tests for POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -81,10 +81,12 @@ UserSchema.statics.findByCredentials = function(email, password){
 
         return new Promise((resolve, reject) => {
             bcrypt.compare(password, user.password, (err,res) => {
-                if(!err){
-                    return resolve(user);
+                if(res){
+                    resolve(user);
                 }
-                return reject();
+                else{
+                    reject("typed password didn't match bcrypted password");
+                }
             })
         })
     })
